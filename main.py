bf2338e266400c1ec4be00f6f5928a7707c67fd7
mmilanes <mmilaneswfk@gmail.com>
2025-05-02 12:15:22 +0200

    Add configuration file and update main script to handle dataset and model building

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import subprocess
+import yaml
+
+# Load configuration from the specified file
+config_path = 'configs/main_config.yml'
+with open(config_path, 'r') as file:
+    config = yaml.safe_load(file)
+
+    # Extract specific configuration values
+    BUILD_DATASET = config['BUILD_DATASET']
+    BUILD_MODEL = config['BUILD_MODEL']
+    USE_SELECTION_IF_AVAILABLE = config['USE_SELECTION_IF_AVAILABLE']
+
+# Print confirmation
+print(f"Configuration loaded from {config_path}")
 
 def main():
     # Path to the virtual environment's Python interpreter
     venv_python = r"Scripts\python.exe"
 
+    # Execute the dataset creation script only when BUIL_DATASET is true
+    if BUILD_DATASET:
+        subprocess.run([venv_python, "./src/create_dataset.py"], check=True)
+    else:
+        print("Dataset creation skipped.")
+    
     # Execute the first script using the venv Python
-    subprocess.run([venv_python, "./src/classification_modeling_single.py"], check=True)
+    if BUILD_MODEL:
+        subprocess.run([venv_python, "./src/classification_modeling_single.py"], check=True)
+    else:
+        print("Model building skipped.")
     
     # Execute the second script using the venv Python
     subprocess.run([venv_python, "./src/classification_backtesting_single.py"], check=True)
